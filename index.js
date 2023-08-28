4ff426950ae2826ea2285c68de38771fda1c0468
Maksymilian <max.szewczyk@interia.pl>
2023-08-28 22:15:11 +0200

    Created Todo App. (HTML, CSS, JS)

---
+++ index.js
@@ -0,0 +1,121 @@
+let todoInput
+let errorInfo
+let addBtn
+let ulList
+
+let popup
+let popupInfo
+let todoToEdit
+let deleteTodoBtn
+let popupInput
+let popupAddBtn
+let popupCloseBtn
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		ulList.append(newTodo)
+		createToolsArea(newTodo)
+
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!'
+	}
+}
+
+const createToolsArea = todo => {
+	const divTools = document.createElement('div')
+	divTools.classList.add('tools')
+	todo.append(divTools)
+
+	const btnComplete = document.createElement('button')
+	btnComplete.classList.add('complete')
+	btnComplete.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+	divTools.append(btnComplete, editBtn, deleteBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = e => {
+	todoToEdit = e.target.closest('li')
+
+	popupInput.value = todoToEdit.firstChild.textContent
+	popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = ''
+}
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz podać jakąś treść!'
+	}
+}
+
+const deleteTodo = e => {
+	deleteTodoBtn = e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście.'
+	}
+}
+
+const enterKeyCheck = e => {
+	if (e.key === 'Enter') {
+		console.log('enter')
+		addNewTodo()
+	}
+}
+
+document.addEventListener('DOMContentLoaded', main)
